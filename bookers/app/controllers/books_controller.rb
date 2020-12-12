ecd438bcee1421fbfce11754c426efdd2d34e608
shunta33 <syuntaochiai@gmail.com>
2020-12-12 15:51:04 +0000

    [Add]投稿フォーム作成

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,23 +1,33 @@
 class BooksController < ApplicationController
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
   def top
   end
 
   def index
+    @book = Book.new
   end
 
   def show
   end
-  
-  def create
-  end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
   get '/' => 'books#top'
   get 'books/' => 'books#index'
+  post '/book' => 'books#create'
   get 'books/show' => 'books/:id'
   get 'books/edit' => 'books/:id/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
