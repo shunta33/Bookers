6f0b5a1a06d319719d115df28bc5b2dbf3a10d23
shunta33 <syuntaochiai@gmail.com>
2020-12-13 15:28:12 +0000

    [Update]投稿データ編集のリダイレクト先を指定

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'books/' => 'books#index'
   post '/book' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book_show'
-  get 'books/:id/edit' => 'books#edit', as: 'book_e'
+  get 'books/:id/edit' => 'books#edit', as: 'book_edit'
+  patch 'books/:id' => 'books#update', as: 'book_update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
