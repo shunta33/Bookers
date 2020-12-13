6ca0c5858a76fa92a7b50de390167582f11faa6f
shunta33 <syuntaochiai@gmail.com>
2020-12-13 14:56:26 +0000

    [Update]詳細ページ

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'books#top'
   get 'books/' => 'books#index'
   post '/book' => 'books#create'
-  get 'books/show' => 'books/:id'
+  get 'books/:id' => 'books#show', as: 'book_show'
   get 'books/edit' => 'books/:id/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
