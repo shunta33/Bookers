a7e23fe8c12fb91501c8f7e68f244156f516dffd
shunta33 <syuntaochiai@gmail.com>
2020-12-13 15:44:38 +0000

    [Add]削除機能の実装

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show', as: 'book_show'
   get 'books/:id/edit' => 'books#edit', as: 'book_edit'
   patch 'books/:id' => 'books#update', as: 'book_update'
+  delete 'books/:id' => 'books#destroy', as: 'book_destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
