60ec023407fa78a5dfe7346720da2cfb1f4bf1b1
shunta33 <syuntaochiai@gmail.com>
2020-12-14 08:16:36 +0000

    [Add]CSSの追加

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'books#top'
   get 'books/' => 'books#index', as: 'book_list'
-  post '/book' => 'books#create'
+  post '/books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book_show'
   get 'books/:id/edit' => 'books#edit', as: 'book_edit'
   patch 'books/:id' => 'books#update', as: 'book_update'
